c463df2cc8297f21cc93501f482238ec35c7c963
liaoyinuo <yinuoliao@outlook.com>
2023-06-04 20:35:07 +0800

    gpt格式

--- packages/utils/convertMessage.ts
+++ packages/utils/convertMessage.ts
@@ -34,7 +34,7 @@ function convertSystemMessage(message: any) {
                 break;
             }
             case 'gpt': {
-                message.content = `:「${content.ask}」#auqa#ChatGPT:#font${content.answer}`;
+                message.content = `:「${content.ask}」<br><font color=auqa>ChatGPT:</font>${content.answer}`;
                 break;
             }
             default: {
