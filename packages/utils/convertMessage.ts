a80f603c080d5b7c129adbed4b7875826e402532
liaoyinuo <yinuoliao@outlook.com>
2023-06-11 14:04:36 +0800

    HTML标签修复3

--- packages/utils/convertMessage.ts
+++ packages/utils/convertMessage.ts
@@ -54,18 +54,18 @@ function convertSystemMessage(message: any) {
 
 function jhconvert(strstr: string){
     return strstr
-    .replace(/</gm, '≺')
-    .replace(/>/gm, '≻')
-    .replace(/x([0-9a-fA-F]{6}|[0-9a-fA-F]{3})#(\S+)/gm, '<font color=$1>$2</font>')
-    .replace(/0#/gm, '<br>')
-    .replace(/1#(\S+)/gm, '<b>$1</b>')
-    .replace(/2#(\S+)/gm ,'<i>$1</i>')
-    .replace(/3#(\S+)/gm, '<u>$1</u>')
-    .replace(/4#(\S+)/gm, '<s>$1</s>')
-    .replace(/red#(\S+)/gm,   '<font color=red>$1</font>')
-    .replace(/blue#(\S+)/gm, '<font color=blue>$1</font>')
-    .replace(/aqua#(\S+)/gm, '<font color=aqua>$1</font>')
-    .replace(/(^@|\W@)(\S+)/gm, '<font color=8A2BE2>@$1</font>');
+    .replace(/</gm, "≺")
+    .replace(/>/gm, "≻")
+    .replace(/x([0-9a-fA-F]{6}|[0-9a-fA-F]{3})#(\S+)/gm, "<font color='$1'>$2</font>")
+    .replace(/0#/gm, "<br>")
+    .replace(/1#(\S+)/gm, "<b>$1</b>")
+    .replace(/2#(\S+)/gm ,"<i>$1</i>")
+    .replace(/3#(\S+)/gm, "<u>$1</u>")
+    .replace(/4#(\S+)/gm, "<s>$1</s>")
+    .replace(/red#(\S+)/gm,   "<font color='red'>$1</font>")
+    .replace(/blue#(\S+)/gm, "<font color='blue'>$1</font>")
+    .replace(/aqua#(\S+)/gm, "<font color='aqua'>$1</font>")
+    .replace(/(^@|\W@)(\S+)/gm, "<font color='8A2BE2'>@$1</font>");
 }
 
 function convertMessageHtml(message: any) {
@@ -79,7 +79,7 @@ function convertMessageHtml(message: any) {
 function convertMessageReply(message: any) {
     if (message.type === 'reply') {
         const content = JSON.parse(message.content);
-        message.content = `<font color=8A2BE2>${content.replywho}:</font>「${content.orignmsg}」<hr>${jhconvert(content.replymsg)}`;
+        message.content = `<font color='8A2BE2'>${content.replywho}:</font>「${content.orignmsg}」<hr>${jhconvert(content.replymsg)}`;
     }
     return message;
 }
