cef23432ed745a70078612ce4ed7e41c1ef1f5e7
liaoyinuo <yinuoliao@outlook.com>
2023-06-11 20:26:16 +0800

    网易云测试

--- packages/web/src/modules/Chat/Message/MusicMessage.tsx
+++ packages/web/src/modules/Chat/Message/MusicMessage.tsx
@@ -8,10 +8,12 @@ interface MusicMessageProps {
 function MusicMessage(props: MusicMessageProps) {
     // eslint-disable-next-line react/destructuring-assignment
     const content = `//music.163.com/outchain/player?type=2&id=${props.content}&auto=1&height=66`;
+    const w=280;
+    const h=86;
 
     return (
         <div className={Style.textMessage} style={{margin:'-10px -12px'}}>
-            <iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width="280" height="86" src={content}></iframe>
+            <iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width={w} height={h} src={content}></iframe>
         </div>
     );
 }
