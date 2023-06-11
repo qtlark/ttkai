4c3dab382cadf70568fd26a656dee6b9f2e48e6a
liaoyinuo <yinuoliao@outlook.com>
2023-06-11 14:43:38 +0800

    空回复优化

--- packages/server/src/routes/message.ts
+++ packages/server/src/routes/message.ts
@@ -196,7 +196,7 @@ export async function sendMessage(ctx: Context<SendMessageData>) {
                 messageContent = JSON.stringify({
                     replywho: regexResult[1].replace(/<[^>]+>/gm, ''),
                     orignmsg: regexResult[2].replace(/<[^>]+>/gm, ''),
-                    replymsg: regexResult[3].trim()?regexResult[3].trim():' ',
+                    replymsg: regexResult[3].trim()?regexResult[3].trim():'　',
                 });
             }
         };
