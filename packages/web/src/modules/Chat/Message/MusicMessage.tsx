5d53b0f908ff5662b50c69d2611dc8e893c5dfb5
liaoyinuo <yinuoliao@outlook.com>
2023-06-12 00:05:07 +0800

    网易云HTML嵌入

--- packages/web/src/modules/Chat/Message/MusicMessage.tsx
+++ packages/web/src/modules/Chat/Message/MusicMessage.tsx
@@ -7,13 +7,10 @@ interface MusicMessageProps {
 
 function MusicMessage(props: MusicMessageProps) {
     // eslint-disable-next-line react/destructuring-assignment
-    const content = `https://music.163.com/outchain/player?type=2&id=${props.content}&auto=0&height=66`;
-    const w=200;
-    const h=86;
+    const content = `<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=200 height=86 src="https://music.163.com/outchain/player?type=2&id=${props.content}&auto=0&height=66"></iframe>`;
 
     return (
-        <div className={Style.textMessage} style={{margin:'-10px -12px -14px -12px'}}>
-            <iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width={w} height={h} src={content}></iframe>
+        <div className={Style.textMessage} style={{margin:'-10px -12px -14px -12px'}} dangerouslySetInnerHTML={{ __html: content }}>
         </div>
     );
 }
