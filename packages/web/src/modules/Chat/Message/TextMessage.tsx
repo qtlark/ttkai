1b093f9cfbb61991ebae466e00d0339f7d97fa5d
liaoyinuo <pyplot@163.com>
2023-07-12 23:26:29 +0800

    正则测试

--- packages/web/src/modules/Chat/Message/TextMessage.tsx
+++ packages/web/src/modules/Chat/Message/TextMessage.tsx
@@ -8,7 +8,13 @@ interface TextMessageProps {
 
 function TextMessage(props: TextMessageProps) {
     // eslint-disable-next-line react/destructuring-assignment
-    const content = jhconvert(props.content);
+    const mcRegex   = /^\w{32}(#\w{32})?#.+#.+$/;
+    
+    var content = props.content;
+
+    if (! mcRegex.test(content)) {
+        content = jhconvert(content);
+    }
 
     return (
         <div
