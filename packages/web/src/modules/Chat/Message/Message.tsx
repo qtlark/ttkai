d45460dd5eeeca19e7f187b564d08dadb5259dab
liaoyinuo <pyplot@163.com>
2023-07-14 11:10:51 +0800

    图片

--- packages/web/src/modules/Chat/Message/Message.tsx
+++ packages/web/src/modules/Chat/Message/Message.tsx
@@ -134,7 +134,7 @@ class Message extends Component<MessageProps, MessageState> {
 
     handleReplyMessage = async () => {
         const { type, content, username, qwe } = this.props;
-        if (type==='text'){
+        if (type==='text'||'image'){
             qwe.current.insertCursor(`回复${username}「${content}」:   `);
         }
         else if (type==='reply'){
