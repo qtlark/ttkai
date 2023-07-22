172fc50e2930fffad32344d9a2abdebd5c5ce0f3
liaoyinuo <pyplot@163.com>
2023-07-22 20:30:09 +0800

    取消图片下载

--- packages/web/src/modules/Chat/Message/ImageMessage.tsx
+++ packages/web/src/modules/Chat/Message/ImageMessage.tsx
@@ -101,11 +101,9 @@ function ImageMessage(props: ImageMessageProps) {
                             {
                                 src: getOSSFileUrl(src, `image/quality,q_95`),
                                 alt: '',
-                                downloadUrl: 'https://ttkai.top/cv' + src
                             },
                         ]}
                         noNavbar
-                        downloadable
                     />
                 )}
             </div>
