38b865b8c31d73881a7de9cbcf44288b311453b0
liaoyinuo <pyplot@163.com>
2023-07-27 15:14:44 +0800

    直播渲染B23dbg

--- packages/server/src/routes/message.ts
+++ packages/server/src/routes/message.ts
@@ -119,9 +119,9 @@ async function shortBV2long(surl) {
     const res = await axios({
         method: 'get',
         url: surl,
-        responseType: 'text',
-        headers: {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'}
+        headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE', 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive'}
     });
+
     assert(res.status === 200, 'bilibili服务端错误');
 
     try {
