416804f1d0e9d41e73104ee75b9425041fe0f53e
liaoyinuo <pyplot@163.com>
2023-07-29 12:45:46 +0800

    图片px修正

--- packages/web/src/modules/Chat/Message/ReplyMessage.tsx
+++ packages/web/src/modules/Chat/Message/ReplyMessage.tsx
@@ -16,8 +16,8 @@ function ReplyMessage(props: ReplyMessageProps) {
     if (imgRegex.test(jscontent.orignmsg)) {
         const regexResult = imgRegex.exec(jscontent.orignmsg);
         const p_width  = 110;
-        const p_height = parseInt(  parseInt(regexResult[2]) * parseInt(regexResult[1]) / p_width  );
-        content = `<font color=8A2BE2>${jhconvert(jscontent.replywho)}</font><div style="text-align: center;"><img src=${jscontent.orignmsg} style="width: ${p_width}px; height: ${p_height}"></div><hr>${jhconvert(jscontent.replymsg)}`;
+        const p_height = parseInt(  parseInt(regexResult[2]) *  p_width / parseInt(regexResult[1]) );
+        content = `<font color=8A2BE2>${jhconvert(jscontent.replywho)}</font><div style="text-align: center;"><img src=${jscontent.orignmsg} style="width: ${p_width}px; height: ${p_height}px"></div><hr>${jhconvert(jscontent.replymsg)}`;
         return (
             <div
                 className={`${Style.textMessage} ${Style.replyimg}`}
