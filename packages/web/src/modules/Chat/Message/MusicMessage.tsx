829b5c8a7cc4f484dcf86e17d763555362ee595b
liaoyinuo <pyplot@163.com>
2023-07-29 13:14:55 +0800

    网易云样式修正

--- packages/web/src/modules/Chat/Message/MusicMessage.tsx
+++ packages/web/src/modules/Chat/Message/MusicMessage.tsx
@@ -7,10 +7,10 @@ interface MusicMessageProps {
 
 function MusicMessage(props: MusicMessageProps) {
     // eslint-disable-next-line react/destructuring-assignment
-    const content = `<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=220 height=86 src="https://music.163.com/outchain/player?type=2&id=${props.content}&auto=0&height=66"></iframe>`;
+    const content = `<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=200 height=86 src="https://music.163.com/outchain/player?type=2&id=${props.content}&auto=0&height=66"></iframe>`;
 
     return (
-        <div className={Style.textMessage} style={{margin:'-10px -12px -14px -12px', height: '90px', width:'220px'}} dangerouslySetInnerHTML={{ __html: content }}>
+        <div className={Style.textMessage} style={{margin:'-10px -12px -14px -12px', height: '90px', width:'200px'}} dangerouslySetInnerHTML={{ __html: content }}>
         </div>
     );
 }
