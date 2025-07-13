816609f91cbac9bd9061551cf942dff9ca8cf720
qtlark <pyplot@163.com>
2025-07-13 17:04:46 +0800

    v

--- packages/web/src/modules/Sidebar/About.tsx
+++ packages/web/src/modules/Sidebar/About.tsx
@@ -20,7 +20,7 @@ function About(props: AboutProps) {
         >
             <div>
                 <div className={Common.block}>
-                    <p className={Common.title}>版本：<font color="red">v25.01.23</font></p>
+                    <p className={Common.title}>版本：<font color="red">v25.07.13</font></p>
                     <ul>
                         <li>欢迎来到ttkai</li>
                         <li>与你的日常，就是「奇迹」</li>
